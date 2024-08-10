544bdabb545381e4121deb9bd5c8ff4e5e9cbf05
JacobG55 <82186653+JacobG55@users.noreply.github.com>
2024-08-10 14:41:21 -0400

    Add files via upload

--- Plugin.cs
+++ Plugin.cs
@@ -10,7 +10,7 @@ namespace JLL
     {
         private const string modGUID = "JacobG5.JLL";
         private const string modName = "JLL";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.1.0";
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -29,6 +29,7 @@ namespace JLL
 
             harmony.PatchAll(typeof(ItemChargerPatch));
             harmony.PatchAll(typeof(TimeOfDayPatch));
+            harmony.PatchAll(typeof(HudManagerPatch));
         }
     }
 }
