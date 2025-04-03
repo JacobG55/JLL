70cc8e5c338a5d55a5c551daeb72db1a4e8f3d15
JacobG55 <82186653+JacobG55@users.noreply.github.com>
2025-04-02 20:04:21 -0400

    1.9.4 Editor Module

---
+++ Modules/EditorModule/EditorScripts/Components/PurgeChildren.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace JLLEditorModule.EditorScripts.Components
+{
+    [ExecuteInEditMode]
+    public class PurgeChildren : MonoBehaviour
+    {
+        public bool Execute = false;
+        public int split = 2;
+
+        public void Update()
+        {
+            if (Execute)
+            {
+                Execute = false;
+
+                GameObject container = new GameObject("Container");
+                container.transform.parent = transform;
+
+                List<Transform> SplitTransforms = new List<Transform>();
+                for (int i = split; i < transform.childCount; i += split)
+                {
+                    SplitTransforms.Add(transform.GetChild(i));
+                }
+
+                foreach (Transform t in SplitTransforms)
+                {
+                    t.SetParent(container.transform);
+                }
+            }
+        }
+    }
+}
